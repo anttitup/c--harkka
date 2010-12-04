0ae3db5109b1fa2743f960449ee04a79b1b75733
AnttiTup <anttitup@cs.helsinki.fi>
2010-12-04 14:28:20 +0200

    m

--- Test.h
+++ Test.h
@@ -1,17 +1,18 @@
 namespace Test
 {
-	class Test
-	{
-		private:
-			int test_number;
-		public:
-			int const& test_number() const;
-			void test_number(int const& newNumber);	
-	}
+    class Test
+    {
+	private:
+	    int test_number;
+	public:
+	    int const& test_number() const;
+	    void test_number(int const& newNumber);
+            template<class T> bool equals(T,T);
+            template<class T> bool less(T,T);
+            template<class T> bool greater(T,T);
+            template<class T> 
+                bool assert_function(T,T,int(*funct)(T,T));
+            template<class T> bool same_type(T,T);
+        }
 
-	template<class T> bool equals(T,T);
-	template<class T> bool less(T,T);
-	template<class T> bool greater(T,T);
-	template<class T> bool assert_function(T,T,int(*funct)(T,T));
-	template<class T> bool same_type(T,T);
 }
